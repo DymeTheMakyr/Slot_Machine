77a38f64836d73978c6c850ed15ff7ae487a7a4d
Dyme <hugosimpson275@gmail.com>
2025-01-06 09:58:12 +0000

    asdsa

---
+++ Slot_Machine/Handlers/PlayerHandler.cs
@@ -0,0 +1,13 @@
+﻿using Exiled.API.Enums;
+using Exiled.API.Features.Spawn;
+using Exiled.Events;
+using Exiled.Events.EventArgs.Player;
+using PluginAPI.Core;
+
+namespace Slot_Machine.Handlers {
+    public class PlayerHandler {
+        public void OnItemDropped(DroppedItemEventArgs ev) {
+            Log.Info(""+ev.Player.Position);
+        }
+    }
+}
---
+++ Slot_Machine/PluginCore.cs
@@ -0,0 +1,30 @@
+﻿using AdminToys;
+using Exiled.API.Features;
+using Exiled.API.Features.Toys;
+using UnityEngine;
+using Log = PluginAPI.Core.Log;
+using Player = Exiled.Events.Handlers.Player;
+
+namespace Slot_Machine {
+    public class PluginCore : Plugin<Config> {
+        public static PluginCore Instance;
+
+        private Handlers.PlayerHandler playerHandler;
+        
+        public override void OnEnabled() {
+            base.OnEnabled();
+            //register events
+            playerHandler = new();
+            Player.DroppedItem += playerHandler.OnItemDropped;
+            //spawn slot machines
+            Primitive CISlot = Primitive.Create(PrimitiveType.Cube, PrimitiveFlags.Collidable, Config.CISlotPos, Vector3.zero, new Vector3(1, 2, 1), true, new Color(0,0,0,0) );
+            Primitive NTFSlot = Primitive.Create(PrimitiveType.Cube, PrimitiveFlags.Collidable, Config.NTFSlotPos, Vector3.zero, new Vector3(1, 2, 1), true, new Color(0,0,0,0) );
+        }
+
+        public override void OnDisabled() {
+            base.OnDisabled();
+            Player.DroppedItem -= playerHandler.OnItemDropped;
+            playerHandler = null;
+        }
+    }
+}
