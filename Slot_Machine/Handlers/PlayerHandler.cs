c88fa64cf7d8a823f910c4611b5d25dff1afe4a1
Dyme <hugosimpson275@gmail.com>
2025-01-09 09:49:50 +0000

    Fixed Slots (Hopefully)

--- Slot_Machine/Handlers/PlayerHandler.cs
+++ Slot_Machine/Handlers/PlayerHandler.cs
@@ -62,10 +62,6 @@ namespace Slot_Machine.Handlers {
         ];
 
         public void OnItemDropped(DroppedItemEventArgs ev) {
-            Timing.RunCoroutine(ProcessItemDropped(ev));
-        }
-
-        public IEnumerator<float> ProcessItemDropped(DroppedItemEventArgs ev) {
             ItemType item = ev.Pickup.Type;  //get item
             Vector3 position = Vector3.negativeInfinity;  //set pos as "null"
             Vector3 rotation = Vector3.negativeInfinity;  //set rot as "null"
@@ -77,18 +73,52 @@ namespace Slot_Machine.Handlers {
                 ev.Pickup.Destroy();
                 result = -1;
                 Log.Info("start");
-                yield return Timing.WaitUntilDone(Timing.RunCoroutine(Gamble(PluginCore.CISlots, x => result = x)));
                 Log.Info(result.ToString());
                 position = Config.CISlotPos;
                 rotation = Quaternion.AngleAxis(PluginCore.CISlots.Body[0].Rotation.y, Vector3.up) * Vector3.forward;
+                Timing.RunCoroutine(Gamble(PluginCore.CISlots, item, position, rotation));
             }
             else if ((ev.Pickup.Position - Config.NTFSlotPos).magnitude < Config.pickupRange) {
                 ev.Pickup.Destroy();
                 result = -1;
-                yield return Timing.WaitUntilDone(Timing.RunCoroutine(Gamble(PluginCore.NTFSlots, x => result = x)));
                 position = Config.NTFSlotPos;
                 rotation = Quaternion.AngleAxis(PluginCore.NTFSlots.Body[0].Rotation.y, Vector3.up) * Vector3.forward;
+                Timing.RunCoroutine(Gamble(PluginCore.NTFSlots, item, position, rotation));
             }
+        }
+        
+        public IEnumerator<float> Gamble(SlotMachine sm, ItemType item ,Vector3 position, Vector3 rotation) {
+            int[] indicies = [Random.Range(0, 8), Random.Range(0, 8), Random.Range(0, 8)];
+            
+            int result = 0;
+            if (indicies[0] == indicies[1] && indicies[0] == indicies[2]) if (indicies[0] == 0) {
+                    result = 2;
+                }
+                else {
+                    result = 1;
+                }
+            //wheels will spin for 4s 1s 1s and will stop at randomly generated index
+            
+            Timing.CallContinuously(4f + 0.063f*indicies[0], () => {
+                for (int i = 0; i < 12; i++) {
+                    sm.Wheels[i].Rotation *= Quaternion.Euler(720f * Timing.DeltaTime, 0f, 0f);
+                }
+            });
+            
+            Timing.CallContinuously(4.5f + 0.063f*indicies[1], () => {
+                for (int i = 4; i < 12; i++) {
+                    sm.Wheels[i].Rotation *= Quaternion.Euler(720f * Timing.DeltaTime, 0f, 0f);
+                }
+            });
+            
+            CoroutineHandle longestSpin = Timing.CallContinuously(5f + 0.063f*indicies[2], () => {
+                for (int i = 8; i < 12; i++) {
+                    sm.Wheels[i].Rotation *= Quaternion.Euler(720f * Timing.DeltaTime, 0f, 0f);
+                }
+            });
+            
+            yield return Timing.WaitUntilDone(longestSpin);
+            
             if (position != Vector3.negativeInfinity && rotation != Vector3.negativeInfinity){
                 if (result == 2) {
                     //play jackpot sound
@@ -118,62 +148,15 @@ namespace Slot_Machine.Handlers {
 
                     int i = tier.IndexOf(item);
                     if (i != tier.Length - 1) {
-                        Item.Create(tier[i + 1]).CreatePickup(position);
+                        i++;
                     }
-                    else {
-                        Item.Create(tier[i]).CreatePickup(position);
-                    }
+                    
+                    Item.Create(tier[i + 1]).CreatePickup(position + rotation);
                 }
                 else {
                     //play fail sound
                 }
             }
-        }
-        
-        public IEnumerator<float> Gamble(SlotMachine sm, Action<int> returnResult) {
-            int[] indicies = new int[3];
-            indicies[0] = Random.Range(0, 8);
-            indicies[1] = Random.Range(0, 8);
-            indicies[2] = Random.Range(0, 8);
-            Log.Info(indicies[0] + " " + indicies[1] + " " + indicies[2]);
-            //wheels will spin for 4s 1s 1s and will stop at randomly generated index
-            
-            yield return Timing.WaitUntilDone(Timing.CallContinuously(4f, () => {
-                for (int i = 0; i < 12; i++) {
-                    sm.Wheels[i].Rotation *= Quaternion.Euler(720f * Timing.DeltaTime, 0f, 0f);
-                }
-            }));
-            
-            while (!Mathf.Approximately(Mathf.Round(sm.Wheels[3].Rotation.eulerAngles.x % 360), indicies[0] * 45f)) {
-                for (int i = 0; i < 12; i++) {
-                    sm.Wheels[i].Rotation *= Quaternion.Euler(720f * Timing.DeltaTime, 0f, 0f);
-                }
-            }
-            
-            yield return Timing.WaitUntilDone(Timing.CallContinuously(0.5f, () => {
-                for (int i = 4; i < 12; i++) {
-                    sm.Wheels[i].Rotation *= Quaternion.Euler(630f * Timing.DeltaTime, 0f, 0f);
-                }
-            }));
-            
-            while (!Mathf.Approximately(Mathf.Round(sm.Wheels[7].Rotation.eulerAngles.x % 360), indicies[1] * 45f)) {
-                for (int i = 4; i < 12; i++) {
-                    sm.Wheels[i].Rotation *= Quaternion.Euler(630f * Timing.DeltaTime, 0f, 0f);
-                }
-            }
-            
-            yield return Timing.WaitUntilDone(Timing.CallContinuously(0.5f, () => {
-                for (int i = 8; i < 12; i++) {
-                    sm.Wheels[i].Rotation *= Quaternion.Euler(540 * Timing.DeltaTime, 0f, 0f);
-                }
-            }));
-            while (!Mathf.Approximately(Mathf.Round(sm.Wheels[11].Rotation.eulerAngles.x % 360), indicies[2] * 45f)) {
-                for (int i = 8; i < 12; i++) {
-                    sm.Wheels[i].Rotation *= Quaternion.Euler(540f * Timing.DeltaTime, 0f, 0f);
-                }
-            }
-
-            returnResult(0);
             yield return 0;
         }
     }
